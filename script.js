981ed719292a289d66b8932c9cc5389a8679d11e
alici69b <aliciaalonsoaguaded@gmail.com>
2026-04-07 09:50:35 +0200

    calculadora terminada

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 //declaro variable 
 let display = document.getElementById('display');
 
+display.textContent = 0;
+
 //agrego una funcion donde el valor que hemos asignado en el html, aparecera en la pantalla que es display
 function agregar(valor) {
 
-    display.textContent += valor;
+    if(display.textContent === "0") {
+        display.textContent = valor;
+    } else {
+        display.textContent += valor;
+    }
+    
+}
+
+//funcion para borrar el contenido y lo ponga vacio
+function borrar() {
+    display.textContent = "";
+}
+
+//funcion para calcular, eval hace el calculo solo 
+function calcular() {
+    display.textContent = eval(display.textContent);
 }
